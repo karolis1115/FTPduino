ce3b8098c732b84e3438bb251a58832b58844d2b
Karolis <karolisjuoz@gmail.com>
2024-02-21 01:32:56 +0100

    Update DownloadFileToBuffer_WiFi.ino

--- examples/DownloadFileToBuffer_WiFi/DownloadFileToBuffer_WiFi.ino
+++ examples/DownloadFileToBuffer_WiFi/DownloadFileToBuffer_WiFi.ino
@@ -5,42 +5,48 @@
 
 FTPduino ftp;
 
-
+//Add your WiFi details here
 static char const SSID[] = "YOUR_SSID";
 static char const PASS[] = "YOUR_PASS";
 
+
+// Add the details of the FTP server you are trying to connect to
 static char const serverIP[] = "FTP_SERVER_IP";
 
 static char const FTPuser[] = "FTP_USERNAME";
 static char const FTPpass[] = "FTP_PASSWORD";
 
 
-static char const workDirr[] = "/";
 
-static char const fileName[] = "HelloWorld.txt";
+static char const workDirr[] = "/";  //dirrectory in which desired file resides in
+
+static char const fileName[] = "HelloWorld.txt";  //Name of desired file
+
 
 void setup() {
   Serial.begin(115200);
 
+  while (!Serial)
+    ;
+
+  //Attempt WiFi connection
   if (!ConnectWiFi(SSID, PASS)) {
     Serial.println("WiFi Connection Failed!");
     while (true)
       ;
   }
+  //if connections successful, print ip address of device
   Serial.println("WiFi connected");
   Serial.println("Local IP address: " + WiFi.localIP());
 
-
-
-
-
+  //Attempt FTP server connection
   if (!ftp.connect(serverIP, 21)) {
     Serial.println("conn failed");
   }
-  Serial.println("conn succ");
+  Serial.println("Connection successful");
 
   ftp.authenticate(FTPuser, FTPpass);
-  Serial.println("auth succ");
+  Serial.println("Authentication successful");
 
   ftp.setWorkDirectory(workDirr);
 
